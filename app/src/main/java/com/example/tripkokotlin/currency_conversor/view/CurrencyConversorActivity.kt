7d325dde475ea1ff4ed81a8c4b13e3b8750d7787
Kilian <kilian.garcia106@alu.ulpgc.es>
2022-01-01 15:24:14 +0000

    Implemented viewModel, model for currencyConversor

--- app/src/main/java/com/example/tripkokotlin/currency_conversor/view/CurrencyConversorActivity.kt
+++ app/src/main/java/com/example/tripkokotlin/currency_conversor/view/CurrencyConversorActivity.kt
@@ -1,15 +1,20 @@
-package com.example.tripkokotlin.currency_conversor
+package com.example.tripkokotlin.currency_conversor.view
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.tripkokotlin.R
+import com.example.tripkokotlin.currency_conversor.viewModel.CurrencyConversorViewModel
 import com.example.tripkokotlin.databinding.ActivityCurrencyConversorBinding
 import com.example.tripkokotlin.main_menu.MainMenuActivity
 
 class CurrencyConversorActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityCurrencyConversorBinding
+
+    private val currencyConversorViewModel : CurrencyConversorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -19,9 +24,17 @@ class CurrencyConversorActivity: AppCompatActivity() {
         binding = ActivityCurrencyConversorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        currencyConversorViewModel.currencyConversorModel.observe(this, Observer {
+
+        })
+
         binding.toolbar.homeImageButton.setOnClickListener(){
             val homeImageIntent = Intent(this, MainMenuActivity::class.java)
             startActivity(homeImageIntent)
+        }
+
+        binding.calculateButton.setOnClickListener(){
+
         }
 
     }
